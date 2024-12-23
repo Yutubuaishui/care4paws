b4987fc9810a427000290cca751371dbac5933ae
Qian Yun <limqianyun03@gmail.com>
2024-12-23 22:57:58 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Chatbot from "./Component/Chatbot";
-import Homepage from "./Component/Homepage";
-
+import './App.css';
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/be-pet-experts" element={<Chatbot />} />
-          <Route path="/view-all-pets" element={<Chatbot />} />
-
-          {/* <Route path="/adopt-pet" component={AdoptPet} />
-          <Route path="/donate" component={Donate} />
-          <Route path="/lost-and-found" component={LostAndFoundPage} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">Hello</div>
   );
 }
+export default App;
