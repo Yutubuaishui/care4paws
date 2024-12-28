304456c0a6f60f4e108dab2340b12df0096862d0
Qian Yun <limqianyun03@gmail.com>
2024-12-28 17:01:18 +0800

    Updated About Us Page

--- src/component/About_Us/About_Us.js
+++ src/component/About_Us/About_Us.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './About_Us.css';
+import AboutUsPageImage from '../../assets/AboutUsPageImage.jpg';
 
 const AboutUs = () => {
     return (
-        <div className="about-us-container">
-            <div className="about-us-left">
-                <h1 class="about-us-title">About Us</h1>
-                <p>
-                    We’re passionate about improving the lives of pets and the people who love them. Through innovative tools and a supportive community, we connect animals in need with caring families, reunite lost pets with their owners, and provide resources to support responsible pet ownership.
-                </p>
-            </div>
-            <div className="about-us-right">
-                {/* Future image or content goes here */}
+        <div className="common-container">
+            <div className="about-us-content">
+                <div className="about-us-left">
+                    <h1 className="about-us-title">About Us</h1>
+                    <p className="about-us-text">
+                        We’re passionate about improving the lives of pets and the people who love them. Through innovative tools and a supportive community, we connect animals in need with caring families, reunite lost pets with their owners, and provide resources to support responsible pet ownership.
+                    </p>
+                </div>
+                <div className="about-us-right">
+                    <img className="about-us-image" src={AboutUsPageImage} alt="About Us" />
+                </div>
             </div>
         </div>
     );
